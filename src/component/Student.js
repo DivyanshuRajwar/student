77198c90a500954241e7a126520bf445349a2238
DivyanshuRajwar <divyanshurajwar@gmail.com>
2024-11-14 08:38:01 +0530

    Build for hosting

--- src/component/Student.js
+++ src/component/Student.js
@@ -20,7 +20,7 @@ function Student() {
     }
     
     try {
-      const response = await axios.post('http://localhost:3000/submit-student-data', studentData);
+      const response = await axios.post('https://server-vpgh.onrender.com/submit-student-data', studentData);
       console.log("Server response:", response.data);
       setName('');
       setRollNo('');
